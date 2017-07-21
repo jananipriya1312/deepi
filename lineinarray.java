2ae468949050b78caffd5b05dd810d0e33a71fcb
jananipriya1312 <janani.priyaeee@ksrce.ac.in>
2017-07-21 16:40:41 +0530

    Add files via upload

---
+++ lineinarray.java
@@ -0,0 +1,40 @@
+package gen;
+import java.util.Scanner;
+public class lineinarray {
+	public static void main(String[] args){
+	 Scanner console = new Scanner(System.in);           
+
+     System.out.println("File to be read: ");
+     String inputFile = console.next();
+
+     File file = new File(inputFile);
+     Scanner in = new Scanner(file);
+
+     int words = 0;
+     int lines = 0;
+     int chars = 0;
+
+     while(in.hasNext())
+     {
+         in.next();
+         words++;
+     }
+
+     while(in.hasNextLine())
+     {
+         in.nextLine();
+         lines++;
+     }
+
+     while(in.hasNextByte())
+     {
+         in.nextByte();
+         chars++;
+     }
+
+     System.out.println("Number of lines: " + lines);
+     System.out.println("Number of words: " + words);
+     System.out.println("Number of characters: " + chars);
+}
+}
+
